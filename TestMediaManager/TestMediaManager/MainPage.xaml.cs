d6095da680816e29f4c9e7e0be137c6dbad28f44
Michael Engelmann <zeprogramfactory@outlook.com>
2019-04-12 22:03:52 +0200

    Bug: XamarinMediaManager 0.5.0

---
+++ TestMediaManager/TestMediaManager/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MediaManager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestMediaManager
+{
+   // Learn more about making custom code visible in the Xamarin.Forms previewer
+   // by visiting https://aka.ms/xamarinforms-previewer
+   [DesignTimeVisible(true)]
+   public partial class MainPage : ContentPage
+   {
+      public MainPage()
+      {
+         InitializeComponent();
+      }
+
+      private async void Button_Clicked(object sender, EventArgs e)
+      {
+         //Audio
+         await CrossMediaManager.Current.Play("https://ia800806.us.archive.org/15/items/Mp3Playlist_555/AaronNeville-CrazyLove.mp3");
+      }
+   }
+}
